3d79c251495e41d25b8531f072da6479c4214c58
VladUngurean <136657499+VladUngurean@users.noreply.github.com>
2024-08-15 21:27:31 +0300

    past events start .nav menu done

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 // Load header
-fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header').innerHTML = data;
-    });
+fetch("header.html")
+    .then((response) => response.text())
+    .then((data) => {
+        document.getElementById("header").innerHTML = data;
+        return fetch("footer.html");
+    })
+    .then((response) => response.text())
+    .then((data) => {
+        document.getElementById("footer").innerHTML = data;
 
-// Load footer
-fetch('footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer').innerHTML = data;
+        // Code to run after header and footer are loaded
+        const hamburger = document.querySelector(".hamburger");
+        const open_header = document.querySelector(".nav_conatainer");
+        const close_button = document.querySelector(".close_button")
+        const body = document.querySelector("body")
+
+        if (hamburger && open_header) {
+            hamburger.addEventListener("click", function () {
+                open_header.classList.toggle("active");
+                body.classList.toggle("active");
+            });
+            close_button.addEventListener("click", function () {
+                open_header.classList.toggle("active");
+                body.classList.toggle("active");
+            });
+        }
+    })
+
+    // Load footer
+fetch("footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+        document.getElementById("footer").innerHTML = data;
     });
