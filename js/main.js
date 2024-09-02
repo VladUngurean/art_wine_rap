af1d909f4e19a9a09fa6aabe2a324bd98ca4ca57
VladUngurean <136657499+VladUngurean@users.noreply.github.com>
2024-09-02 20:10:36 +0300

    better img loading

--- js/main.js
+++ js/main.js
@@ -36,3 +36,20 @@ fetch("footer.html")
 
 
 //scroll 
+//img blur load 
+
+let blurDivs = document.querySelectorAll(".blur_img_load")
+
+blurDivs.forEach(div => {
+    const img = div.querySelector("img")
+
+    function loaded() {
+        div.classList.add("loaded")
+    }
+
+    if (img.complete) {
+        loaded()
+    } else {
+        img.addEventListener("load", loaded)
+    }
+})
